0c75dfffeee2d6072134b2817df9c0e358300d68
jai-sharma <sendtojsharma@gmail.com>
2021-03-12 09:03:49 +0530

    added global styled

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
-import 'styles/globals.css';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  :root {
+      font-size: 62.5%;
+  }
+  
+  body {
+    margin: 0;
+    padding: 0;
+
+    font-size: 1.6rem;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+                Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+const theme = {
+    colors: {
+        primary: '#0070f3',
+    },
+};
+
+export default function App({ Component, pageProps }) {
+    return (
+        <>
+            <GlobalStyle />
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    );
 }
-
-export default MyApp;
